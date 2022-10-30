15acd64d08570ee180a22757d9c99b6ab54550c8
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2022-10-30 19:21:50 +0530

    changes

---
+++ src/containers/home/Cards.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Cardcontainer, Card } from './right.style'
+
+const Cards = () => {
+  return (
+    <Cardcontainer>
+        <Card>
+            
+        </Card>
+    </Cardcontainer>
+  )
+}
+
+export default Cards
